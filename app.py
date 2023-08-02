ce47c05758848d3e4cf5c896de4fd640605d540f
manojn1729 <manoj.n1729@gmail.com>
2023-08-02 12:11:13 +0530

    new upgraded version of app with memory

--- app.py
+++ app.py
@@ -3,45 +3,101 @@ import openai
 import pyodbc
 import os
 import pandas as pd
-from dotenv import load_dotenv
 from myfunctions import *
+import time
 
-load_dotenv()
 
-openai.api_key=os.getenv('key')
 
-with st.sidebar:
-    if x:=st.text_input('Table name',placeholder='Type table name'):
-        st.session_state['tableName']=x
+if 'status' not in st.session_state:
+    st.session_state['status']={
+        'db':False,
+        'ai':False
+    }
 
 if "messages" not in st.session_state:
      st.session_state['messages'] = []
 
-if "tableName" not in st.session_state:
-    st.session_state['tableName']=None
+if "tableNames" not in st.session_state:
+    st.session_state['tableNames']=[]
 
-if "cursur" not in st.session_state:
-    st.session_state['cnxn'], st.session_state['cursor']=setup_cursor() 
+
+if "tables" not in st.session_state:
+    st.session_state['tables']=[]
 
 for message in st.session_state.messages:
     x=user_chat (message['content']) if message['role']=='user' else assistant_chat (message['content'])
 
+
+with st.sidebar:
+
+    if x:=st.multiselect('Select tables',options=st.session_state['tables']):
+        st.session_state['tableNames']=x
+
+    with st.form('config'):
+
+        DRIVER=st.selectbox('Server Type',['MYSQL'])
+        SERVER=st.text_input('Server')
+        DATABASE=st.text_input('DataBase')
+        UID=st.text_input('User Name')
+        PASSWORD=st.text_input('Password',type='password')
+        key=st.text_input('API key',type='password')
+        if DRIVER=='MYSQL':
+            DRIVER='MySQL ODBC 8.0 ANSI Driver'
+        if submit:=st.form_submit_button('submit'):
+            st.session_state['setup']={
+            'DRIVER':DRIVER,
+            'SERVER':SERVER,
+            'UID':UID,
+            'DATABASE':DATABASE,
+            'PASSWORD':PASSWORD,
+            'key':key
+                    }
+
+            openai.api_key=st.session_state['setup']['key']
+
+            if not check_key():
+                st.session_state['status']['ai']=False
+            else:
+                st.session_state['status']['ai']=True
+    
+            try:
+                connect_db()
+            except:
+                st.session_state['status']['db']=False
+            else:
+                st.session_state['status']['db']=True
+                st.session_state['tables']=list_tables()
+                st.experimental_rerun()
+
+        if st.session_state['status']['db']:
+            st.success('connected db')
+        else:
+            st.error('provide valid db credentials')
+        
+        if st.session_state['status']['ai']:
+            st.success('valid API key')
+        else:
+            st.error('provide valid api key')
+
 prompt=st.chat_input (placeholder='ask to query on RDBMS') 
+
 if prompt:
-    if st.session_state['tableName']:
-        user_chat (f' {prompt}')
-        response=openai.Completion.create(
-        model="text-davinci-003",
-        prompt="generate single Mysql query for the Givn DDL  "+generate_ddl(st.session_state['tableName'],st.session_state['cnxn'])+"/t"+prompt,
-        max_tokens=250,
-        temperature=0.3
-        )
-
-        assistant_chat(response.choices[0].text.replace('\n', ' ')) 
-
-        st.dataframe(sql_excqute (response.choices[0].text.replace('\n', ' '), st.session_state['cnxn']),hide_index=True,use_container_width=True)
-
-        st.session_state.messages.append({'role': 'user', 'content': prompt}) 
-        st.session_state.messages.append({'role': 'ai', 'content': response.choices[0].text.replace('\n', ' ')})
+    if check_status():
+        if st.session_state['tableNames']:
+            try:
+                user_chat(prompt)
+                result=chat_openai(prompt)
+                st.session_state.messages.append({'role': 'user', 'content': prompt}) 
+                st.session_state.messages.append({'role': 'assistant', 'content':result})
+                if query:=get_code(result):
+                    try:
+                        st.dataframe(sql_excqute (query.replace('\n', ' ')),hide_index=True,use_container_width=True)
+                    except TypeError:
+                        st.success('done')
+                    st.session_state['tables']=list_tables()
+            except Exception as e:
+                st.error(e)
+        else:
+            st.warning('please select one or more table')
     else:
-        st.warning('please enter the table name')
+        st.error('please provide valide credentials')
--- myfunctions.py
+++ myfunctions.py
@@ -1,7 +1,9 @@
 import streamlit as st 
+import re
 import pyodbc
 import os
 import pandas as pd
+import openai
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,24 +13,117 @@ def user_chat (message):
 
 def assistant_chat (message):
     with st.chat_message("assistant", avatar='🤖'):
-        st.code(message, language='sql')
+        st.write(message)
+
+# def setup():
+#     st.session_state['setup']={
+#         'DRIVER':os.getenv('DRIVER'),
+#         'SERVER':os.getenv('SERVER'),
+#         'UID':os.getenv('UID'),
+#         'DATABASE':os.getenv('DATABASE'),
+#         'PASSWORD':os.getenv('PASSWORD'),
+#         'key':os.getenv('key')
+#     }
 
 def setup_cursor():
-    cnxn = pyodbc.connect("DRIVER={{{0}}}; SERVER={1};DATABASE={2}; UID={3}; PASSWORD={4};TransactionIsolationLevel=READ UNCOMMITTED;".format(os.getenv('DRIVER'),
-                                                                                             os.getenv('SERVER'),
-                                                                                             os.getenv('DATABASE'),
-                                                                                             os.getenv('UID'),
-                                                                                             os.getenv('PASSWORD')))
+    cnxn = pyodbc.connect("DRIVER={{{0}}}; SERVER={1};DATABASE={2}; UID={3}; PASSWORD={4};TransactionIsolationLevel=READ UNCOMMITTED;".format(
+       st.session_state['setup']['DRIVER'],
+       st.session_state['setup']['SERVER'],
+       st.session_state['setup']['DATABASE'],
+       st.session_state['setup']['UID'],
+       st.session_state['setup']['PASSWORD']))
     return cnxn,cnxn.cursor()
 
-def sql_excqute (query, cnxn):
-    return pd.read_sql (query, cnxn)
+def sql_excqute (query):
+    return pd.read_sql (query,st.session_state['cnxn'])
 
-def generate_ddl(table_name,cnxn):
-    df=pd.read_sql (f"describe {table_name}", cnxn)
-    query=f"Create table {table_name} ("
-    for i,row in df.iterrows():
-        query+=f"{row['Field']} {row['Type']}, "
-    query=query[:-2]
-    query+="}"
-    return query
+
+def chat_openai(prompt):
+  
+    full_response=''
+    with st.chat_message("assistant", avatar='🤖'):
+        message=[{"role":"system","content":"Given the following SQL tables, your job is to write MySQL queries given by a user’s request wrap inside ```sql.\n\n"+generate_ddl(st.session_state['tableNames'])}]
+        message+=message+formate_sql(st.session_state.messages[-4:]).copy()+[{'role': 'user', 'content':prompt}]
+        message_placeholder = st.empty()
+        full_response = ""
+        for i in openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=message,
+        temperature=0,
+        max_tokens=1024,
+        top_p=1,
+        n=1,
+        stream=True
+    ):
+
+            full_response += i.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    
+    return full_response
+    
+
+def generate_ddl(table_names):
+    response=''
+    for table in table_names:
+        df = sql_excqute(f"describe {table}")
+        columns = [f"{row['Field']} {row['Type']}" for _, row in df.iterrows()]
+        primary_keys = [row['Field'] for _, row in df.iterrows() if row['Key'] == 'PRI']
+        primary_key_constraint = f", primary key ({','.join(primary_keys)})" if primary_keys else ''
+        df=sql_excqute(f'''SELECT 
+        TABLE_NAME,COLUMN_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME
+        FROM
+        INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+        WHERE
+        REFERENCED_TABLE_SCHEMA = 'sakila' AND
+        TABLE_NAME = '{table}';''')
+        forigin_key=[f"FOREIGN KEY ({row['COLUMN_NAME']}) REFERENCES {row['REFERENCED_TABLE_NAME']}({row['REFERENCED_COLUMN_NAME']})" for _,row in df.iterrows()]
+        forigin_key_constraint=f", {','.join(forigin_key)}" if forigin_key else ''
+        query=f"CREATE TABLE {table} ({', '.join(columns)}{primary_key_constraint}{forigin_key_constraint})\n\n"
+        response+=query
+    
+    return response
+
+
+def get_code(message):
+    response=[]
+    pattern='```sql([\s\S]*?)```'
+    match=re.findall(pattern,message)
+    if len(match)!=0: return match[0] 
+
+def formate_sql(messages):
+    pattern='```sql([\s\S]*?)```'
+    for i,message in enumerate(messages):
+        if message['role']!='user':
+            temp=re.findall(pattern,message['content'])
+            if len(temp)!=0:
+                tstring=''
+                for templ1 in temp:
+                    tstring=tstring+'```sql'+templ1+'```'+'\n'
+                messages[i]['content']=tstring
+    return messages
+
+def list_tables():
+    df=sql_excqute('show tables')
+    return df.iloc[:,0].values
+                
+
+
+
+def connect_db():
+    st.session_state['cnxn'], st.session_state['cursor']=setup_cursor()
+    st.session_state['tables']=list_tables()
+
+
+def check_key():
+  try:
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="",
+    max_tokens=1)
+  except:
+    return False
+  return True
+
+def check_status():
+    return st.session_state['status']['db'] and st.session_state['status']['ai']
